74bf377c8273218115846200f26428b8d7eb6f46
kirill.sergeev <kirill.sergeev@progforce.com>
2025-02-07 10:21:56 +0300

    counter example

---
+++ src/components/Board.tsx
@@ -0,0 +1,71 @@
+import Square from "./Square";
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
+function calculateTurns(squares) {
+  return squares.filter((square) => !square).length;
+}
+
+function calculateStatus(winner, turns, player) {
+  if (!winner && !turns) return "Draw";
+  if (winner) return `Winner ${winner}`;
+  return `Next player: ${player}`;
+}
+
+export default function Board({ xIsNext, squares, onPlay }) {
+  const winner = calculateWinner(squares);
+  const turns = calculateTurns(squares);
+  const player = xIsNext ? "X" : "O";
+  const status = calculateStatus(winner, turns, player);
+
+  function handleClick(i) {
+    if (squares[i] || winner) return;
+    const nextSquares = squares.slice();
+    nextSquares[i] = player;
+    onPlay(nextSquares);
+  }
+
+  return (
+    <>
+      <div style={{ marginBottom: "0.5rem" }}>{status}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateRows: "repeat(3, 1fr)",
+          width: "calc(3 * 2.5rem)",
+          height: "calc(3 * 2.5rem)",
+          border: "1px solid #999",
+        }}
+      >
+        {squares.map((_, i) => (
+          <Square
+            key={`square-${i}`}
+            value={squares[i]}
+            onSquareClick={() => handleClick(i)}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
